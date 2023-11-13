d22243ad3a201d52c63b8f29b858fbb2de09287c
EmilAbb <emilabbasov775@gmail.com>
2023-11-13 10:29:22 +0400

    changes

--- main.py
+++ main.py
@@ -2,34 +2,31 @@ import requests
 import urllib.parse
 import re
 
-token = "95834b64978546429116076e72b179174796bd22aad"
-targetUrls = ["https://jugaad.az", "https://westtown.az", "https://kbt.az"]
+token = "901f258707514402968d954be89ce390a01ac0275c4"
+base_url = "https://yellowpages.az/Az/results/page/{}/?search-title=Cihaz&sort-by=asc"
+email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 
-with open("email_results.txt", "w") as file:
-    for i, targetUrl in enumerate(targetUrls, start=1):
+with open("email_results.txt", "a") as file:  # Open the file in "append" mode ("a")
+    page = 1
+    while True:
+        targetUrl = base_url.format(page)
         encoded_url = urllib.parse.quote(targetUrl)
         url = "http://api.scrape.do?token={}&url={}".format(token, encoded_url)
-        response = requests.request("GET", url)
+        response = requests.get(url)
 
-        print("URL {}: {}".format(i, targetUrl))
+        print("Page {}: {}".format(page, targetUrl))
         html_content = response.text
 
-
-        email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
         emails = re.findall(email_pattern, html_content)
 
         if emails:
             print("Email Addresses:")
             for email in emails:
                 print(email)
-        else:
-            print("Email address not found.")
-
-
-        file.write("URL {}: {}\n".format(i, targetUrl))
-        if emails:
-            file.write("Email Addresses:\n")
-            for email in emails:
                 file.write(email + "\n")
-        else:
-            file.write("Email address not found.\n")
+
+        # Manually increment the page number or change the URL structure if necessary
+        page += 1
+
+        if page > 10:  # Change this number to control how many pages you want to scrape
+            break
